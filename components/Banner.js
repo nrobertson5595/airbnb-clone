433b70ff3a39c9a148bde1860718fb3807abbdcb
Nick <nrobertson5595@gmail.com>
2021-12-28 10:11:06 -0500

    updated banner scale

--- components/Banner.js
+++ components/Banner.js
@@ -8,8 +8,8 @@ const Banner = () => {
         layout="fill"
         objectFit="cover"
       />
-      <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+      <div className="sm:absolute top-1/2 w-full text-center ">
+        <p className=" text-sm sm:text-lg">Not sure where to go? Perfect.</p>
         <button
           className="text-purple-500 bg white px-10 py-4 shadow-md rounded-full font-bold my-3
         hover:shadow-xl active:scale-90 transition duration-150
